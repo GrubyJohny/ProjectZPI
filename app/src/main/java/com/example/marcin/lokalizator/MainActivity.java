a2c54a7d5b57819c029e9ba05cf0921f461de085
Wariat92 <karoltomczyk92@gmail.com>
2015-02-27 19:07:44 +0100

    Pierwszy test Obczaj to ZIOM !!

--- app/src/main/java/com/example/marcin/lokalizator/MainActivity.java
+++ app/src/main/java/com/example/marcin/lokalizator/MainActivity.java
@@ -20,7 +20,9 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        System.out.println("dsadsa");
         return true;
+
     }
 
     @Override

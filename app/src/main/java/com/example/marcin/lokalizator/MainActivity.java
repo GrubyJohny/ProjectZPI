96dde51c6769d1f90aa3f2a81257cef6a4ae2f1d
sanczo_nb <sanczo16@gmail.com>
2015-03-01 20:44:26 +0100

    oponent

--- app/src/main/java/com/example/marcin/lokalizator/MainActivity.java
+++ app/src/main/java/com/example/marcin/lokalizator/MainActivity.java
@@ -12,8 +12,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Hello Chudy!");
-        System.out.println("SIEMA");
+        System.out.println("Hello Chum=dy!");
+        System.out.println("S");
+        System.out.println("Nana nana");
     }
 
 

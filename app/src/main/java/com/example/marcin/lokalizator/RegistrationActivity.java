8b47766a680dea0ba52f8ff9293708a43d349b84
slawkow <szczurek.slawek@gmail.com>
2015-03-18 02:36:44 +0100

    dodana rejestracja i logowanie wstępne, do tego grid wydzielony

--- app/src/main/java/com/example/marcin/lokalizator/RegistrationActivity.java
+++ app/src/main/java/com/example/marcin/lokalizator/RegistrationActivity.java
@@ -6,21 +6,20 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class RegistrationActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registration);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_registration, menu);
         return true;
-
     }
 
     @Override

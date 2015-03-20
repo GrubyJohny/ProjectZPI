3c063660aa1904810c7e5b7ab2489e0eff20fe7f
slawkow <szczurek.slawek@gmail.com>
2015-03-20 15:48:13 +0100

    zakladki

--- app/src/main/java/com/example/marcin/lokalizator/MainActivity.java
+++ app/src/main/java/com/example/marcin/lokalizator/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.location.LocationManager;
+import android.widget.TabHost;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -22,6 +23,28 @@ public class MainActivity extends ActionBarActivity {
         if (! manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
             buildAlertMessageNoGps();
         }
+        tabs();
+    }
+
+    public void tabs(){
+        TabHost tabHost=(TabHost)findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        TabHost.TabSpec spec1=tabHost.newTabSpec("Tab 1");
+        spec1.setContent(R.id.tab1);
+        spec1.setIndicator("Tab 1");
+
+        TabHost.TabSpec spec2=tabHost.newTabSpec("Tab 2");
+        spec2.setIndicator("Tab 2");
+        spec2.setContent(R.id.tab2);
+
+        TabHost.TabSpec spec3=tabHost.newTabSpec("Tab 3");
+        spec3.setIndicator("Tab 3");
+        spec3.setContent(R.id.tab3);
+
+        tabHost.addTab(spec1);
+        tabHost.addTab(spec2);
+        tabHost.addTab(spec3);
     }
 
     private void buildAlertMessageNoGps() {

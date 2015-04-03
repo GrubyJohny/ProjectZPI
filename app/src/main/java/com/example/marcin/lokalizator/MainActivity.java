30b5bb6b8637da38c1b4640ab4f1a6029dc74d0c
slawkow <szczurek.slawek@gmail.com>
2015-04-04 01:16:28 +0200

    dodane kółko, ale nie ustawione, bo mam z tym problem ... jak znajde czas to poczytam jak umieścić tam zdjęcie/awatar ... we fliperze jako druga strona jest lista znajomych, tylko, nie mam pojęcia dlaczego się nie wyświetla (może na emulatorze problem ?) ... brałem stąd: http://www.learn2crack.com/2013/10/android-custom-listview-images-text-example.html ... nie ma co zepsuć, a mi się nie pokazuje
    ponadto usunięte przyciski do mapy i wylogowywania, log out już działa spod kółka

--- app/src/main/java/com/example/marcin/lokalizator/MainActivity.java
+++ app/src/main/java/com/example/marcin/lokalizator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.marcin.lokalizator;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -13,10 +14,17 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.AdapterViewFlipper;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.ViewFlipper;
 
 import com.google.android.gms.maps.GoogleMap;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -24,15 +32,17 @@ public class MainActivity extends ActionBarActivity {
     SessionManager session;
     ViewFlipper vieFli;
     private float lastX;
+    private Spinner spinner1;
+    private Button circleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        final LocationManager manager = (LocationManager) getSystemService( Context.LOCATION_SERVICE );
+        final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        if (! manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
             buildAlertMessageNoGps();
         }
 
@@ -43,20 +53,28 @@ public class MainActivity extends ActionBarActivity {
         vieFli.setHorizontalScrollBarEnabled(true);
 
 
-
-        View vie = View.inflate(this, R.layout.activity_my_map, null );
+        View vie = View.inflate(this, R.layout.activity_my_map, null);
         //View vie = View.inflate(this, R.layout.activity_login, null);
         vieFli.addView(vie);
 
-        View vie2 = View.inflate(this, R.layout.activity_register, null);
+        View vie2 = View.inflate(this, R.layout.activity_friends, null);
         vieFli.addView(vie2);
 
-        View vie3 = View.inflate(this, R.layout.activity_login, null );
+        //View vie3 = View.inflate(this, R.layout.activity_login, null);
         //View vie = View.inflate(this, R.layout.activity_login, null);
-        vieFli.addView(vie3);
+        //vieFli.addView(vie3);
+
+        spinner1 = (Spinner) findViewById(R.id.spinner);
+        String[] spinnerOptions = {"Settings", "Log out"};
+        ArrayAdapter<String> circleButtonOptions = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, spinnerOptions);
+        spinner1.setAdapter(circleButtonOptions);
+
+        circleButton = (Button) findViewById(R.id.circleButton);
+
+        addListenerOnButton();
+        addListenerOnSpinner();
 
     }
-
 
 
     private void buildAlertMessageNoGps() {
@@ -102,30 +120,25 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void logOut(View view) {
+    public void logOut(AdapterView.OnItemSelectedListener view) {
         session.setLogin(false);
         Intent closeIntent = new Intent(this, LoginActivity.class);
         startActivity(closeIntent);
     }
 
     // Method to handle touch event like left to right swap and right to left swap
-    public boolean onTouchEvent(MotionEvent touchevent)
-    {
-        switch (touchevent.getAction())
-        {
+    public boolean onTouchEvent(MotionEvent touchevent) {
+        switch (touchevent.getAction()) {
             // when user first touches the screen to swap
-            case MotionEvent.ACTION_DOWN:
-            {
+            case MotionEvent.ACTION_DOWN: {
                 lastX = touchevent.getX();
                 break;
             }
-            case MotionEvent.ACTION_UP:
-            {
+            case MotionEvent.ACTION_UP: {
                 float currentX = touchevent.getX();
 
                 // if left to right swipe on screen
-                if (lastX < currentX)
-                {
+                if (lastX < currentX) {
                     // If no more View/Child to flip
                     if (vieFli.getDisplayedChild() == 0)
                         break;
@@ -137,8 +150,7 @@ public class MainActivity extends ActionBarActivity {
                 }
 
                 // if right to left swipe on screen
-                if (lastX > currentX)
-                {
+                if (lastX > currentX) {
                     if (vieFli.getDisplayedChild() == 1)
                         break;
 
@@ -153,11 +165,43 @@ public class MainActivity extends ActionBarActivity {
         return false;
     }
 
-    public void openMap(View v)
-    {
+    public void openMap(View v) {
         Intent i = new Intent(getApplicationContext(),
                 MyMapActivity.class);
         startActivity(i);
         finish();
+    }
+
+    public void addListenerOnButton() {
+
+        circleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                spinner1.performClick();
+            }
+        });
+
+    }
+
+    public void addListenerOnSpinner(){
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch((int)position)
+                {
+                    case 0:
+                        //USTAWIENIA TO DO
+                        break;
+                    case 1:
+                        logOut(this);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }

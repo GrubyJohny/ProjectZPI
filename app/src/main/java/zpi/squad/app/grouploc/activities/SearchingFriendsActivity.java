8f9901c0113a3144cb4a8ce92bc1fe3cc813a15e
slawkow <szczurek.slawek@gmail.com>
2016-02-29 00:19:15 +0100

    dodawanie znajomych po kliknięciu w 'plusa'

--- app/src/main/java/zpi/squad/app/grouploc/activities/SearchingFriendsActivity.java
+++ app/src/main/java/zpi/squad/app/grouploc/activities/SearchingFriendsActivity.java
@@ -11,20 +11,8 @@ import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.Filter;
 import android.widget.ListView;
-import android.widget.Toast;
-
-import com.parse.ParseException;
-import com.parse.ParseInstallation;
-import com.parse.ParseObject;
-import com.parse.ParsePush;
-import com.parse.ParseQuery;
-import com.parse.ParseUser;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import zpi.squad.app.grouploc.R;
 import zpi.squad.app.grouploc.SessionManager;
@@ -36,13 +24,8 @@ public class SearchingFriendsActivity extends AppCompatActivity {
     ListView searchFriendListView;
     public static SearchingFriendAdapter adapter;
     ArrayList<Friend> searchFriendsList;
-    ParseQuery<ParseUser> query, queryFriend;
-    ParseQuery queryAlreadyFriends, queryAlreadyFriends2;
-    ParseUser newFriend = null;
-    boolean alreadyFriends = false, alreadySent = false, success = false;
     private SessionManager session = SessionManager.getInstance();
     View empty;
-    private int selectedItem = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,21 +44,11 @@ public class SearchingFriendsActivity extends AppCompatActivity {
 
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
                 item = (Friend) searchFriendListView.getItemAtPosition(position);
-
-                try {
-                    selectedItem = position;
-                    new SendFriendshipRequest().execute(item);
-                } catch (Exception e) {
-                    e.getLocalizedMessage();
-                    e.printStackTrace();
-                }
             }
         });
 
         searchFriendInput = (EditText) findViewById(R.id.search_friends_input);
         searchFriendInput.addTextChangedListener(new TextWatcher() {
-
-
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
             }
@@ -98,7 +71,6 @@ public class SearchingFriendsActivity extends AppCompatActivity {
         fill.execute();
     }
 
-
     @Override
     public void onContentChanged() {
         super.onContentChanged();
@@ -106,146 +78,6 @@ public class SearchingFriendsActivity extends AppCompatActivity {
         empty = findViewById(R.id.emptyTextInSearching);
         ListView list = (ListView) findViewById(R.id.searchingFriendsListView);
         list.setEmptyView(empty);
-    }
-
-    private String[] addFriendship(final String newFriendEmail) {
-        String methodResult[] = null;
-        success = false;
-        alreadyFriends = false;
-        alreadySent = false;
-
-        if (newFriendEmail.equals(ParseUser.getCurrentUser().getEmail())) {
-            methodResult = new String[]{"You can't be friend with yourself"};
-        } else {
-
-            ArrayList<Friend> tempFriendsList = SessionManager.getInstance().getFriendsList();
-            List<ParseObject> arrayAlreadyFriends = null;
-            List<ParseObject> arrayAlreadyFriends2 = null;
-
-            if (queryAlreadyFriends != null) queryAlreadyFriends.clearCachedResult();
-            if (queryAlreadyFriends2 != null) queryAlreadyFriends2.clearCachedResult();
-            if (queryFriend != null) queryFriend.clearCachedResult();
-
-            queryFriend = ParseUser.getQuery().whereEqualTo("email", newFriendEmail);
-
-            queryAlreadyFriends = new ParseQuery("Friendship");
-            queryAlreadyFriends.whereEqualTo("friend1", ParseUser.getCurrentUser());
-
-            queryAlreadyFriends2 = new ParseQuery("Friendship");
-            queryAlreadyFriends2.whereEqualTo("friend2", ParseUser.getCurrentUser());
-
-            for (int i = 0; i < tempFriendsList.size(); i++) {
-                if (tempFriendsList.get(i).getEmail().equals(newFriendEmail))
-                    alreadyFriends = true;
-            }
-
-
-            List<ParseUser> list = null;
-            try {
-                list = queryFriend.find();
-
-                if (list.size() > 0) {
-                    newFriend = list.get(0).fetch();
-
-                    arrayAlreadyFriends = queryAlreadyFriends.find();
-
-                    if (arrayAlreadyFriends != null) {
-                        for (int i = 0; i < arrayAlreadyFriends.size(); i++) {
-
-                            if (newFriendEmail.equals(((ParseUser) (arrayAlreadyFriends.get(i)).get("friend2")).fetchIfNeeded().getEmail())) {
-                                if ((arrayAlreadyFriends.get(i)).get("accepted").toString().equals("true"))
-                                    alreadyFriends = true;
-                                else
-                                    alreadySent = true;
-                            }
-
-                        }
-                    }
-
-
-                    arrayAlreadyFriends2 = queryAlreadyFriends2.find();
-
-                    if (arrayAlreadyFriends2 != null) {
-                        for (int i = 0; i < arrayAlreadyFriends2.size(); i++) {
-
-                            if (newFriendEmail.equals(((ParseUser) (arrayAlreadyFriends2.get(i)).get("friend1")).fetchIfNeeded().getEmail())) {
-                                if ((arrayAlreadyFriends2.get(i)).get("accepted").toString().equals("true"))
-                                    alreadyFriends = true;
-                                else
-                                    alreadySent = true;
-
-
-                            }
-                        }
-
-                    }
-
-                    if (!alreadyFriends && !alreadySent) {
-                        ParseObject friendship = new ParseObject("Friendship");
-                        friendship.put("friend1", ParseUser.getCurrentUser());
-                        friendship.put("friend2", newFriend);
-                        friendship.put("accepted", false);
-
-                        friendship.save();
-                        friendship.fetch();
-
-                        success = true;
-                        Log.e("Friendship: ", friendship.getObjectId());
-                        methodResult = new String[]{("Invitation sent to " + newFriend.get("name").toString()), friendship.getObjectId()};
-
-                    } else if (alreadySent)
-                        methodResult = new String[]{"Invitation not responded yet"};
-                }
-            } catch (ParseException e) {
-                e.getLocalizedMessage();
-                e.printStackTrace();
-            }
-
-        }
-
-        Log.e("ROZMIAR RESULTU: ", methodResult == null ? "NULL" : methodResult[0]);
-        return methodResult;
-    }
-
-
-    private void sendFriendshipNotification(String email, String friendName, String friendshipId) throws JSONException {
-        Log.e("do sendFriendshiNotif", "tak");
-        String notificationId = null;
-        try {
-            ParseObject notific = new ParseObject("Notification");
-            notific.put("senderEmail", ParseUser.getCurrentUser().getEmail());
-            notific.put("receiverEmail", newFriend.getEmail());
-            notific.put("kindOfNotification", 101);
-            notific.put("markedAsRead", false);
-            notific.put("extra", friendshipId);
-
-
-            notific.save();
-            notific.fetch();
-            notificationId = notific.getObjectId();
-            Log.e("Notification object: ", "saved succesfully in Parse");
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        ParseQuery notificationQuery = ParseInstallation.getQuery().whereEqualTo("name", email);
-        ParsePush notification = new ParsePush();
-        notification.setQuery(notificationQuery);
-        notification.setMessage("Użytkownik " + ParseUser.getCurrentUser().get("name") + " wysłał Ci zaproszenie do znajomych!");
-
-        JSONObject message = new JSONObject();
-        message.put("kind_of_notification", 101);
-        message.put("friend_email", email);
-        message.put("friendship_id", friendshipId);
-        message.put("new_friend_name", friendName);
-        message.put("notification_id", notificationId);
-
-        notification.setData(message);
-        //notification.setExpirationTimeInterval(60 * 60 * 24 * 7); //1 week
-        notification.sendInBackground();
-
-
-        Log.e("z sendFriendshiNotif", "tak");
     }
 
     private class FillTheList extends AsyncTask<Void, Void, Void> {
@@ -267,44 +99,6 @@ public class SearchingFriendsActivity extends AppCompatActivity {
             super.onPostExecute(aVoid);
 
             adapter.notifyDataSetChanged();
-        }
-    }
-
-    private class SendFriendshipRequest extends AsyncTask<Friend, Void, Void> {
-
-        String[] temp;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-
-        }
-
-        @Override
-        protected Void doInBackground(Friend... params) {
-            if (params[0] != null) {
-                temp = addFriendship(params[0].getEmail());
-                if (temp.length > 1 && temp[0].contains("Invitation sent to")) {
-                    try {
-                        sendFriendshipNotification(params[0].getEmail(), params[0].getName(), temp[1]);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-
-                }
-
-            } else {
-                Log.e("Wrong argument ", " in doInBackground: null");
-
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-
-            Toast.makeText(getApplicationContext(), temp[0], Toast.LENGTH_LONG).show();
         }
     }
 }
--- app/src/main/java/zpi/squad/app/grouploc/adapters/SearchingFriendAdapter.java
+++ app/src/main/java/zpi/squad/app/grouploc/adapters/SearchingFriendAdapter.java
@@ -2,6 +2,8 @@ package zpi.squad.app.grouploc.adapters;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,12 +12,24 @@ import android.widget.Filter;
 import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseInstallation;
+import com.parse.ParseObject;
+import com.parse.ParsePush;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import zpi.squad.app.grouploc.R;
 import zpi.squad.app.grouploc.SessionManager;
+import zpi.squad.app.grouploc.activities.MainActivity;
 import zpi.squad.app.grouploc.domains.Friend;
 import zpi.squad.app.grouploc.helpers.CommonMethods;
 
@@ -25,8 +39,11 @@ public class SearchingFriendAdapter extends ArrayAdapter<Friend> implements Filt
     private SessionManager session = SessionManager.getInstance();
     private ArrayList<Friend> items;
     private ArrayList<Friend> filteredList = new ArrayList<>();
-    //private ArrayList<Friend> alreadyFriendsList ;
-    boolean alreadyIsFriend = false;
+    ParseQuery<ParseUser> queryFriend;
+    ParseQuery queryAlreadyFriends, queryAlreadyFriends2;
+    ParseUser newFriend = null;
+    boolean alreadyFriends = false, alreadySent = false, success = false;
+    private ImageView inviteFriendButton;
 
     public SearchingFriendAdapter(Context context, ArrayList<Friend> items) {
         super(context, R.layout.search_friend_list_row, items);
@@ -36,9 +53,9 @@ public class SearchingFriendAdapter extends ArrayAdapter<Friend> implements Filt
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         // Get the data item for this position
-        Friend friend = getItem(position);
+        final Friend friend = getItem(position);
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.search_friend_list_row, parent, false);
@@ -48,6 +65,13 @@ public class SearchingFriendAdapter extends ArrayAdapter<Friend> implements Filt
         } else {
             convertView.setBackgroundColor(Color.WHITE);
         }
+        inviteFriendButton = (ImageView) convertView.findViewById(R.id.inviteFriend);
+        inviteFriendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new SendFriendshipRequest().execute(friend);
+            }
+        });
         // Lookup view for data population
         TextView name = (TextView) convertView.findViewById(R.id.txt);
         TextView email = (TextView) convertView.findViewById(R.id.userEmail);
@@ -78,34 +102,10 @@ public class SearchingFriendAdapter extends ArrayAdapter<Friend> implements Filt
                     else
                         filteredList.clear();
 
-                    for (int a = 0; a < helpList.size(); a++)
+                    for (int a = 0; a < helpList.size(); a++) {
                         if (helpList.get(a).getName().toLowerCase().contains(constraint.toString().trim().toLowerCase()))
                             filteredList.add(helpList.get(a));
-
-
-                   /* try {
-                        query = ParseUser.getQuery();
-                        query.whereContains("name_lowercase", constraint.toString().trim().toLowerCase());
-
-                        usersFromQuery = query.find();        //lista wszystkich userów spełniających warunki query - czyli nazwa pasująca do wpisanego tekstu
-
-                        for (int i = 0; i < usersFromQuery.size(); i++) {
-                            alreadyIsFriend = false;        //czy aktualnie jest już w znajomych, bo jeśli tak, to nie powinien się znaleźć w "filteredList"
-                            temp = usersFromQuery.get(i).fetchIfNeeded();                     // i-ty element listy wynikwoej query
-
-                            for (Friend f : helpList)
-                                if (f.getEmail().equals(temp.getEmail()))   //jeśli i-ty element list wynikowej query znajduje się już na liście znajomych
-                                    alreadyIsFriend = true;                 //to znaczy, że jest już naszym znajomym
-
-                            if (!alreadyIsFriend)                           //jeśli nie, trafia do listy filteredList
-                                filteredList.add(new Friend(temp.getObjectId(), temp.get("name").toString(), temp.getEmail(), (temp.get("photo").toString()), ((ParseGeoPoint) temp.get("location")).getLatitude(), ((ParseGeoPoint) temp.get("location")).getLongitude()));
-
-                        }
-
-                    } catch (Exception e) {
-                        e.getLocalizedMessage();
-                        e.printStackTrace();
-                    }*/
+                    }
 
                     result.values = filteredList;
                     result.count = filteredList.size();
@@ -127,5 +127,169 @@ public class SearchingFriendAdapter extends ArrayAdapter<Friend> implements Filt
         items.clear();
         items.addAll(values);
         notifyDataSetChanged();
+    }
+
+    private class SendFriendshipRequest extends AsyncTask<Friend, Void, Void> {
+
+        String[] temp;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Friend... params) {
+            if (params[0] != null) {
+                temp = addFriendship(params[0].getEmail());
+                if (temp.length > 1 && temp[0].contains("Invitation sent to")) {
+                    try {
+                        sendFriendshipNotification(params[0].getEmail(), params[0].getName(), temp[1]);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else {
+                Log.e("Wrong argument ", " in doInBackground: null");
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            Toast.makeText(MainActivity.context, temp[0], Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void sendFriendshipNotification(String email, String friendName, String friendshipId) throws JSONException {
+        Log.e("do sendFriendshiNotif", "tak");
+        String notificationId = null;
+        try {
+            ParseObject notific = new ParseObject("Notification");
+            notific.put("senderEmail", ParseUser.getCurrentUser().getEmail());
+            notific.put("receiverEmail", newFriend.getEmail());
+            notific.put("kindOfNotification", 101);
+            notific.put("markedAsRead", false);
+            notific.put("extra", friendshipId);
+
+
+            notific.save();
+            notific.fetch();
+            notificationId = notific.getObjectId();
+            Log.e("Notification object: ", "saved succesfully in Parse");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        ParseQuery notificationQuery = ParseInstallation.getQuery().whereEqualTo("name", email);
+        ParsePush notification = new ParsePush();
+        notification.setQuery(notificationQuery);
+        notification.setMessage("Użytkownik " + ParseUser.getCurrentUser().get("name") + " wysłał Ci zaproszenie do znajomych!");
+
+        JSONObject message = new JSONObject();
+        message.put("kind_of_notification", 101);
+        message.put("friend_email", email);
+        message.put("friendship_id", friendshipId);
+        message.put("new_friend_name", friendName);
+        message.put("notification_id", notificationId);
+
+        notification.setData(message);
+        //notification.setExpirationTimeInterval(60 * 60 * 24 * 7); //1 week
+        notification.sendInBackground();
+
+        Log.e("z sendFriendshiNotif", "tak");
+    }
+
+    private String[] addFriendship(final String newFriendEmail) {
+        String methodResult[] = null;
+        success = false;
+        alreadyFriends = false;
+        alreadySent = false;
+
+        if (newFriendEmail.equals(ParseUser.getCurrentUser().getEmail())) {
+            methodResult = new String[]{"You can't be friend with yourself"};
+        } else {
+
+            ArrayList<Friend> tempFriendsList = SessionManager.getInstance().getFriendsList();
+            List<ParseObject> arrayAlreadyFriends = null;
+            List<ParseObject> arrayAlreadyFriends2 = null;
+
+            if (queryAlreadyFriends != null) queryAlreadyFriends.clearCachedResult();
+            if (queryAlreadyFriends2 != null) queryAlreadyFriends2.clearCachedResult();
+            if (queryFriend != null) queryFriend.clearCachedResult();
+
+            queryFriend = ParseUser.getQuery().whereEqualTo("email", newFriendEmail);
+
+            queryAlreadyFriends = new ParseQuery("Friendship");
+            queryAlreadyFriends.whereEqualTo("friend1", ParseUser.getCurrentUser());
+
+            queryAlreadyFriends2 = new ParseQuery("Friendship");
+            queryAlreadyFriends2.whereEqualTo("friend2", ParseUser.getCurrentUser());
+
+            for (int i = 0; i < tempFriendsList.size(); i++) {
+                if (tempFriendsList.get(i).getEmail().equals(newFriendEmail))
+                    alreadyFriends = true;
+            }
+
+            List<ParseUser> list = null;
+            try {
+                list = queryFriend.find();
+
+                if (list.size() > 0) {
+                    newFriend = list.get(0).fetch();
+
+                    arrayAlreadyFriends = queryAlreadyFriends.find();
+
+                    if (arrayAlreadyFriends != null) {
+                        for (int i = 0; i < arrayAlreadyFriends.size(); i++) {
+
+                            if (newFriendEmail.equals(((ParseUser) (arrayAlreadyFriends.get(i)).get("friend2")).fetchIfNeeded().getEmail())) {
+                                if ((arrayAlreadyFriends.get(i)).get("accepted").toString().equals("true"))
+                                    alreadyFriends = true;
+                                else
+                                    alreadySent = true;
+                            }
+                        }
+                    }
+
+                    arrayAlreadyFriends2 = queryAlreadyFriends2.find();
+
+                    if (arrayAlreadyFriends2 != null) {
+                        for (int i = 0; i < arrayAlreadyFriends2.size(); i++) {
+
+                            if (newFriendEmail.equals(((ParseUser) (arrayAlreadyFriends2.get(i)).get("friend1")).fetchIfNeeded().getEmail())) {
+                                if ((arrayAlreadyFriends2.get(i)).get("accepted").toString().equals("true"))
+                                    alreadyFriends = true;
+                                else
+                                    alreadySent = true;
+                            }
+                        }
+                    }
+
+                    if (!alreadyFriends && !alreadySent) {
+                        ParseObject friendship = new ParseObject("Friendship");
+                        friendship.put("friend1", ParseUser.getCurrentUser());
+                        friendship.put("friend2", newFriend);
+                        friendship.put("accepted", false);
+
+                        friendship.save();
+                        friendship.fetch();
+
+                        success = true;
+                        Log.e("Friendship: ", friendship.getObjectId());
+                        methodResult = new String[]{("Invitation sent to " + newFriend.get("name").toString()), friendship.getObjectId()};
+                    } else if (alreadySent)
+                        methodResult = new String[]{"Invitation not responded yet"};
+                }
+            } catch (ParseException e) {
+                e.getLocalizedMessage();
+                e.printStackTrace();
+            }
+        }
+        Log.e("ROZMIAR RESULTU: ", methodResult == null ? "NULL" : methodResult[0]);
+
+        return methodResult;
     }
 }

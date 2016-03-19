21d4916cbdac33b92bb14db2e7ceea1dc632ecb4
slawkow <szczurek.slawek@gmail.com>
2016-03-19 16:52:13 +0100

    fix do animacji przy wysyłaniu zaproszenia

--- app/src/main/java/zpi/squad/app/grouploc/adapters/SearchingFriendAdapter.java
+++ app/src/main/java/zpi/squad/app/grouploc/adapters/SearchingFriendAdapter.java
@@ -142,12 +142,13 @@ public class SearchingFriendAdapter extends ArrayAdapter<Friend> implements Filt
 
         String[] temp;
         Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.rotate);
+        final ImageView inviteButton = inviteFriendButton;
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
 
-            inviteFriendButton.startAnimation(animation);
+            inviteButton.startAnimation(animation);
         }
 
         @Override
@@ -170,9 +171,9 @@ public class SearchingFriendAdapter extends ArrayAdapter<Friend> implements Filt
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
-            inviteFriendButton.clearAnimation();
-            inviteFriendButton.setBackgroundResource(R.drawable.plus_circle_gray);
-            inviteFriendButton.setClickable(false);
+            inviteButton.clearAnimation();
+            inviteButton.setBackgroundResource(R.drawable.plus_circle_gray);
+            inviteButton.setClickable(false);
             Toast.makeText(MainActivity.context, temp[0], Toast.LENGTH_LONG).show();
         }
     }
